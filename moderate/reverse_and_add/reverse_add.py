02dd442363615e46daee6f31189cc9e309233bcd
MikeDelaney <mdelaney@utexas.edu>
2014-08-26 17:05:49 -0700

    Solve reverse and add

---
+++ moderate/reverse_and_add/reverse_add.py
@@ -0,0 +1,31 @@
+import sys
+
+
+def reverse_add(number):
+    count = 0
+    while not is_palindrome(number) and count < 100:
+        number += reverse_number(number)
+        count += 1
+    return '{0} {1}'.format(count, number)
+
+
+def reverse_number(number):
+    reversed_num = 0
+    num = number
+    while num > 0:
+        digit = num % 10
+        reversed_num = reversed_num * 10 + digit
+        num = num / 10
+    return reversed_num
+
+
+def is_palindrome(number):
+    return number == reverse_number(number)
+
+
+if __name__ == '__main__':
+    with open(sys.argv[1], 'r') as f:
+        for line in f:
+            line = line.rstrip()
+            if line:
+                print reverse_add(int(line))
